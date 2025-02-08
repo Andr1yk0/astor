2c4e9f41c679231212d20dd33923525a77cc6b12
Andriy Lozynskiy <alozynskiy@gmail.com>
2025-02-08 17:53:59 +0200

    update build

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, '../dist'),
+  publicPath: process.env.NODE_ENV === 'production' ? '/astor/dist' : '/',
 })
